b65722e7ec61f92ff8841fbe09cab83b8782a34d
sachin <baralisachin441@gmail.com>
2023-02-07 21:41:32 +0545

    expanse tracker reactapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpanseItem from "./components/ExpanseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpanseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpanseItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ExpanseItem = () => {
+  return (
+    <>
+      <div>Feburary,7,2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$209.99</div>
+      </div>
+    </>
+  )
+}
+
+export default ExpanseItem
